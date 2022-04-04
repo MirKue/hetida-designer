48cc753628be2d000311c6b87e77212855b867cd
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2022-04-04 17:04:43 +0200

    add write and remove code file methods,
    remove unused load_data method

--- runtime/hetdesrun/utils.py
+++ runtime/hetdesrun/utils.py
@@ -1,5 +1,6 @@
 """Utilities for scripting and in particular component/workflow deployment"""
 
+import os
 import json
 from typing import List, Optional, Tuple, Any, Dict
 from enum import Enum
@@ -70,54 +71,6 @@ def get_uuid_from_seed(seed_str: str) -> UUID:
     """
     random.seed(seed_str)
     return UUID(int=random.getrandbits(128))
-
-
-def load_data(
-    json_file: str, md_file: str, code_file: Optional[str] = None
-) -> Tuple[Optional[dict], Optional[str], Optional[str]]:
-    """Loads structured and unstructured component / workflow data from files
-
-    Helper function to load a bunch of data from
-    * 3 files for a component (a json file, the documentation markdown file, the code file)
-    * or from 2 files for a workflow (a json file, the documentation markdown file,).
-
-    Args:
-        json_file (str): path to the json file
-        md_file (str): path to the documentation markdown file
-        code_file (Optional[str], optional): Path to code file. Defaults to None. If None
-            this function will only load the json file and the doc file.
-
-    Returns:
-        Union[Tuple[Any, str], Tuple[Any, str, str]]: A tuple with structured data
-        from json file as first entry, documentation as second entry and if code_file
-        is not None the code as third entry. If some part of loading failes, None is returned
-    """
-    with open(json_file, "r", encoding="utf8") as f:
-        try:
-            info = json.load(f)
-        except json.JSONDecodeError:
-            logger.error("Could not decode %s", json_file)
-            info = None
-    doc: Optional[str]
-    try:
-        with open(md_file, "r", encoding="utf8") as f:
-            doc = f.read()
-    except FileNotFoundError:
-        logger.error("Could not find documentation markdonw file %s", md_file)
-        doc = None
-
-    if code_file is None:
-        return info, doc, None
-
-    code: Optional[str]
-    try:
-        with open(code_file, "r", encoding="utf8") as f:
-            code = f.read()
-    except FileNotFoundError:
-        logger.error("Could not find code file %s", code_file)
-        code = None
-
-    return info, doc, code
 
 
 class State(str, Enum):
@@ -195,3 +148,26 @@ def criterion_unset_or_matches_value(
     if criterion is None:
         return True
     return bool(actual_value == criterion)
+
+
+def write_code_files(path: str) -> None:
+    for root, _, files in os.walk(path):
+        for file in files:
+            path = os.path.join(root, file)
+            if path.endswith("json"):
+                with open(path, "r", encoding="utf-8") as f:
+                    transformation_revision = json.load(f)
+                if transformation_revision["type"] == Type.COMPONENT:
+                    code_file = ".".join(path.split(".")[:-1]) + ".py"
+                    with open(code_file, "w", encoding="utf8") as f:
+                        f.write(transformation_revision["content"])
+
+
+def remove_code_files(path: str) -> None:
+    for root, _, files in os.walk(path):
+        for file in files:
+            path = os.path.join(root, file)
+            if path.endswith("py"):
+                json_file = ".".join(path.split(".")[:-1]) + ".json"
+                if os.path.isfile(json_file):
+                    os.remove(path)
